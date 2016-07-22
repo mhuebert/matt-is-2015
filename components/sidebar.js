47ffbfe65c7673cd31e706f59926831516c0c096
Matthew Huebert <no-email@gmail.com>
2016-07-22 13:56:52 +0200

    Mention ClojureScript

--- components/sidebar.js
+++ components/sidebar.js
@@ -7,7 +7,7 @@ module.exports = React.createClass({
       <div className="paper text-small blue-links" >
         <h2><Link className="no-blue" to="/">Matt Huebert</Link></h2>
         <p>
-          Hey—welcome to my corner of the web. I’m a designer and programmer with a special interest in creative, cross-field collaboration. 
+          Hey—welcome to my corner of the web. I’m a designer and programmer with a special interest in creative, cross-field collaboration. &nbsp; 
           <Link to="/meta">Bio ›</Link>
         </p>
         <hr />
@@ -19,8 +19,9 @@ module.exports = React.createClass({
         <hr />
         <h3>Tech</h3>
         <p>
-          <span className="tag-noclick">Javascript</span>
+          <span className="tag-noclick">JavaScript</span>
           <span className="tag-noclick">React.js</span>
+          <span className="tag-noclick">Lisp</span>
         </p>
         <hr />
         <h3>History</h3>
